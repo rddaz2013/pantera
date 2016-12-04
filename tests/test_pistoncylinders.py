ad580e6a463c98036623ae989f3fd1a528ceaf43
C Reid <charlesreid1@Gmail.com>
2016-12-04 13:39:50 -0800

    Update test_pistoncylinders.py

--- tests/test_pistoncylinders.py
+++ tests/test_pistoncylinders.py
@@ -17,7 +17,7 @@ def test_PistonCylinder_specify_contents():
     """
     Testing initialization of piston cylinder configuration with user-specified contents
     """
-    T = 798.15
+    T = 998.15
     P = pt.one_atm
     X = "CH4:1.5, O2:3.0"
     g = get_gas(T,P,X)
@@ -31,7 +31,7 @@ def test_PistonCylinder_shorthand_specify_contents():
     """
     Testing shorthand initialization of piston cylinder configuration with Cantera-style contents specification
     """
-    T = 798.15
+    T = 998.15
     P = pt.one_atm
     X = "CH4:1.5, O2:3.0"
     g = get_gas(T,P,X)
@@ -50,12 +50,12 @@ def test_PistonCylinder_specify_contents_environment():
     """
     Testing initialization of PistonCylinder with user-specified contents and environment
     """
-    T = 798.15
+    T = 998.15
     P = pt.one_atm
     X = "CH4:1.5, O2:3.0"
     g = get_gas(T,P,X)
 
-    Te = 798.15
+    Te = 998.15
     Pe = pt.one_atm
     Xe = "N2:0.79, O2:0.21"
     e = get_gas(Te,Pe,Xe)
@@ -72,7 +72,7 @@ def test_PistonCylinder_specify_contents_inputparams():
     """
     Testing initialization of PistonCylinder with user-specified contents and input param dict
     """
-    T = 798.15
+    T = 998.15
     P = pt.one_atm
     X = "CH4:1.5, O2:3.0"
     g = get_gas(T,P,X)
@@ -92,148 +92,3 @@ def test_PistonCylinder_specify_contents_inputparams():
     assert pc.params['dummyvar2']==100.0
 
 
-'''
-def test_IsobaricPC_empty():
-    """
-    Testing initialization of IsobaricPC with no inputs
-    """
-    pc = pt.IsobaricPCConfig()
-
-
-
-def test_IsobaricPC_specify_contents():
-    """
-    Testing initialization of IsobaricPC with user-specified contents
-    """
-    T = 798.15
-    P = pt.one_atm
-    X = "CH4:1.5, O2:3.0"
-    g = get_gas(T,P,X)
-
-    pc = pt.PistonCylinderConfig(contents=g)
-
-    assert np.allclose(pc.T,T)
-    assert np.allclose(pc.P,P)
-
-
-
-def test_IsobaricPC_specify_contents_environment():
-    """
-    Testing initialization of IsobaricPC with user-specified contents and environment
-    """
-    T = 798.15
-    P = pt.one_atm
-    X = "CH4:1.5, O2:3.0"
-    g = get_gas(T,P,X)
-
-    Te = 798.15
-    Pe = pt.one_atm
-    Xe = "N2:0.79, O2:0.21"
-    e = get_gas(Te,Pe,Xe)
-
-    # create reactor
-    pc = pt.IsobaricPCConfig(contents=g,environment=e)
-
-    assert np.allclose(pc.T,T)
-    assert np.allclose(pc.P,P)
-
-
-
-def test_IsobaricPC_specify_contents_inputparams():
-    """
-    Testing initialization of IsobaricPC with user-specified contents and input param dict
-    """
-    T = 798.15
-    P = pt.one_atm
-    X = "CH4:1.5, O2:3.0"
-    g = get_gas(T,P,X)
-
-    input_params = {'dummyvar1':10.0,'dummyvar2':100.0}
-
-    # create reactor
-    pc = IsobaricPCConfig(contents=g,params=input_params)
-
-    assert np.allclose(pc.T,T)
-    assert np.allclose(pc.P,P)
-
-    c = pc._contents
-    assert c!=None
-
-    assert pc.params['dummyvar1']==10.0
-    assert pc.params['dummyvar2']==100.0
-
-
-
-def test_IsochoricPC_empty():
-    """
-    Testing initialization of IsochoricPC with no inputs
-    """
-    pc = IsochoricPCConfig()
-
-
-
-def test_IsochoricPC_specify_contents():
-    """
-    Testing initialization of IsochoricPC with user-specified contents
-    """
-    T = 798.15
-    P = pt.one_atm
-    X = "CH4:1.5, O2:3.0"
-    g = get_gas(T,P,X)
-
-    pc = IsochoricPCConfig(contents=g)
-
-    assert np.allclose(pc.T,T)
-    assert np.allclose(pc.P,P)
-
-
-
-def test_IsochoricPC_specify_contents_environment():
-    """
-    Testing initialization of IsochoricPC with user-specified contents and environment
-    """
-    T = 798.15
-    P = pt.one_atm
-    X = "CH4:1.5, O2:3.0"
-    g = get_gas(T,P,X)
-
-    Te = 798.15
-    Pe = pt.one_atm
-    Xe = "N2:0.79, O2:0.21"
-    e = get_gas(Te,Pe,Xe)
-
-    # create reactor
-    pc = pt.IsochoricPCConfig(contents=g,environment=e)
-
-    print pc.T
-    assert np.allclose(pc.T,T)
-    assert np.allclose(pc.P,P)
-
-
-
-def test_IsochoricPC_specify_contents_inputparams():
-    """
-    Testing initialization of IsochoricPC with user-specified contents and input param dict
-    """
-    T = 798.15
-    P = pt.one_atm
-    X = "CH4:1.5, O2:3.0"
-    g = get_gas(T,P,X)
-
-    input_params = {'dummyvar1':10.0,'dummyvar2':100.0}
-
-    # create reactor
-    pc = pt.IsochoricPCConfig(contents=g,params=input_params)
-
-    assert np.allclose(pc.T,T)
-    assert np.allclose(pc.P,P)
-
-    c = pc._contents
-    assert c!=None
-
-    assert pc.params['dummyvar1']==10.0
-    assert pc.params['dummyvar2']==100.0
-
-
-
-'''
